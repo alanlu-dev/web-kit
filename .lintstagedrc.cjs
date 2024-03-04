78cc88a887981963ad24d97ed072c596b4744664
Alan Lu <hi@alanlu.dev>
2024-03-05 04:34:40 +0800

    chore: add cspell check (#4)

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  '*': 'cspell --no-progress --no-must-find-files',
   '*.json': ['eslint --fix', 'prettier --write'],
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.vue': ['eslint --fix', 'prettier --write'],
