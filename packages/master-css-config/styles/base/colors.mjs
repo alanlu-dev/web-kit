121194959462fe1510b92eb7d1f07a5657444256
Alan Lu <hi@alanlu.dev>
2024-04-14 23:07:50 +0800

    feat(master-css-config): update colors

--- packages/master-css-config/styles/base/colors.mjs
+++ packages/master-css-config/styles/base/colors.mjs
@@ -3,6 +3,11 @@ export default {
   primary: '#0384C3',
   secondary: '#F0C453',
   accent: '#EB4949',
+  footer: '#3A87A0',
+  gray: '#A0A0A0',
+  base: {
+    bg: '#FFFFFF',
+  },
   font: {
     title: '#596375',
     content: '#1F2B39',
--- packages/master-css-config/styles/base/variables.mjs
+++ packages/master-css-config/styles/base/variables.mjs
@@ -17,12 +17,12 @@ export default {
     'screen': { max: 1920 },
 
     'box-shadow': {
-      xs: '0|1|2|0|black/.05',
-      sm: '0|1|3|0|black/.1,0|1|2|-1|black/.1',
-      md: '0|4|6|-1|black/.1,0|2|4|-2|black/.1',
-      lg: '0|10|15|-3|black/.1,0|4|6|-4|black/.1',
-      xl: '0|20|25|-5|black/.1,0 8|10|-6|black/.1',
-      inner: 'inset|0|2|4|0|black/.05',
+      xs: '0|1|2|0|gray/.05',
+      sm: '0|1|3|0|gray/.25,0|1|2|-1|gray/.25',
+      md: '0|4|6|-1|gray/.25,0|2|4|-2|gray/.25',
+      lg: '0|10|15|-3|gray/.25,0|4|6|-4|gray/.25',
+      xl: '0|20|25|-5|gray/.25,0 8|10|-6|gray/.25',
+      inner: 'inset|0|2|4|0|gray/.05',
       none: '0|0|#0000',
     },
 
