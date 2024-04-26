58c321ce0c627ec0769b95b4860a51e98429b6bd
Alan Lu <hi@alanlu.dev>
2024-04-27 04:20:36 +0800

    fix(notion-api-zod-schema): update NotionPageSchema properties

---
+++ .changeset/dull-bikes-wait.md
@@ -0,0 +1,5 @@
+---
+"@alanlu-dev/notion-api-zod-schema": patch
+---
+
+fix(notion-api-zod-schema): update NotionPageSchema properties
--- packages/notion-api-zod-schema/src/NotionPageSchema.ts
+++ packages/notion-api-zod-schema/src/NotionPageSchema.ts
@@ -1,20 +1,15 @@
 import { z } from 'zod'
 import type { NotionBlockType } from './NotionBlockSchema'
-import { NotionEmojiSchema, NotionTextSchema } from './NotionTextSchema'
+import { NotionEmojiSchema } from './NotionTextSchema'
 import { NotionObjectSchema } from './NotionObjectSchema'
 import { NotionExternalFileObjectSchema, NotionFileSchema, NotionInternalFileObjectSchema } from './NotionFileSchema'
+import { NotionDatabaseCellSchema, NotionDatabasePropertySchema, NotionTitleSchema } from './NotionDatabaseSchema'
 
 export const NotionPageSchema = NotionObjectSchema.extend({
   object: z.literal('page'),
   cover: NotionFileSchema.nullable(),
   icon: z.discriminatedUnion('type', [NotionEmojiSchema, NotionInternalFileObjectSchema, NotionExternalFileObjectSchema]).nullish(),
-  properties: z.object({
-    title: z.object({
-      id: z.string(),
-      type: z.literal('title'),
-      title: z.array(NotionTextSchema),
-    }),
-  }),
+  properties: z.record(z.union([NotionDatabaseCellSchema.merge(NotionTitleSchema), NotionDatabasePropertySchema])),
 })
 export type NotionPageType = z.infer<typeof NotionPageSchema>
 
