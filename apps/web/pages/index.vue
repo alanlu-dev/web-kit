f259fe08ae69f934cd6d62c17aa8b6d7558eca28
Alan Lu <hi@alanlu.dev>
2024-04-26 23:01:08 +0800

    style(web): 發布標籤不斷行

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -45,7 +45,7 @@ const courses = [
         <Splide :options="{ arrows: false, autoplay: true, interval: 5000, type: 'loop' }">
           <SplideSlide v-for="image in images" :key="image.圖片">
             <nuxt-link :to="image.導轉連結 || undefined" class="rel">
-              <NotionTag :color="image.發布狀態.color" class="abs p:1x rbl:2x right:0 top:0">{{ image.發布狀態.name }}</NotionTag>
+              <NotionTag :color="image.發布狀態.color" class="abs nowrap p:1x rbl:2x right:0 top:0">{{ image.發布狀態.name }}</NotionTag>
               <img :src="image.圖片" :alt="image.標題" :title="image.標題" />
             </nuxt-link>
           </SplideSlide>
