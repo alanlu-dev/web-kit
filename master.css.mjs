04cc746a816e20d5a51519a81158f250a3fe0c17
Alan Lu <hi@alanlu.dev>
2024-05-08 22:29:08 +0800

    chore(deps): bump package versions

--- master.css.mjs
+++
@@ -1,7 +0,0 @@
-import config from '@jiehousekeeper/master-css-config'
-import splide from '@jiehousekeeper/master-css-config/vendors/splide'
-
-/** @type {import('@master/css').Config} */
-export default {
-  extends: [config, splide],
-}
