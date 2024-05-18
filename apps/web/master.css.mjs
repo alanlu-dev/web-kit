8ddffc8828ea8ec90d11f5ffdf16604b79322c97
Alan Lu <hi@alanlu.dev>
2024-05-19 02:20:08 +0800

    style(web): formkit 調整顏色

--- apps/web/master.css.mjs
+++ apps/web/master.css.mjs
@@ -6,7 +6,14 @@ export default {
   extends: [config, splide],
   styles: {
     formkit: {
-      input: '$fk-bg-submit-hover:$(primary) $fk-bg-submit:$(primary) $fk-border-radius:6px',
+      outer: `
+        $fk-bg-submit-hover:$(primary)
+        $fk-bg-submit:$(primary)
+        $fk-border-box-shadow-focus:0|0|0|$(fk-border-width-focus)|$(fk-color-border-focus)
+        $fk-border-radius:6px
+        $fk-border-width-focus:1px
+        $fk-color-border-focus:$(primary)
+      `,
     },
   },
 }
