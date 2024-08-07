bffa9c7117722f42deb26c2153f90360012b8a8e
Alan Lu <hi@alanlu.dev>
2024-08-08 04:53:26 +0800

    fix: 成功才發信

--- apps/web/server/api/payment/receive/[order_page_id].post.ts
+++ apps/web/server/api/payment/receive/[order_page_id].post.ts
@@ -61,54 +61,62 @@ export default defineEventHandler<{
       children: [{ paragraph: { rich_text: [{ text: { content: data ? JSON.stringify(data) : 'null' } }] } }],
     })
 
-    const parsedPage = NotionPageSchema.parse(page)
-    const order = OrderSchema.parse(parsedPage.properties)
-    order.課程安排資訊 = await getCourseEventByIdAsync(notion, order.課程安排ID!)
+    if (data?.RtnCode === '1') {
+      const parsedPage = NotionPageSchema.parse(page)
+      const order = OrderSchema.parse(parsedPage.properties)
+      order.課程安排資訊 = await getCourseEventByIdAsync(notion, order.課程安排ID!)
 
-    const html = await render(
-      MyTemplate,
-      {
-        siteUrl: process.env.NUXT_PUBLIC_SITE_URL,
-        courseName: order.課程安排資訊?.課程資訊?.課程名稱,
-        courseLink: `${process.env.NUXT_PUBLIC_SITE_URL}/course_event/${order.課程安排ID}`,
-        studentName: maskName(order.會員名稱),
-        orderNumber: order.訂單編號,
-        paymentAmount: `NT$ ${formatThousand(order.付款金額!)}`,
-        paymentDate: new Date().toLocaleDateString('zh-TW', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-        }),
-        courseDate: order.課程安排資訊!.上課日期![0]!,
-        courseTime: order.課程安排資訊!.上課日期![1]!,
-        // 課程⽇期減 7 天
-        courseDateMinus7: new Date(new Date(order.課程安排資訊!.上課日期![2]!).getTime() - 7 * 24 * 60 * 60 * 1000).toLocaleDateString('zh-TW', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-        }),
-        courseLocation: order.課程安排資訊?.教室資訊?.地址,
-        logoSrc: `${process.env.NUXT_PUBLIC_SITE_URL}/about/jie_housekeeper.png`,
-      },
-      {
-        pretty: true,
-      },
-    )
+      try {
+        const html = await render(
+          MyTemplate,
+          {
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL,
+            courseName: order.課程安排資訊?.課程資訊?.課程名稱,
+            courseLink: `${process.env.NUXT_PUBLIC_SITE_URL}/course_event/${order.課程安排ID}`,
+            studentName: maskName(order.會員名稱),
+            orderNumber: order.訂單編號,
+            paymentAmount: `NT$ ${formatThousand(order.付款金額!)}`,
+            paymentDate: new Date().toLocaleDateString('zh-TW', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            }),
+            courseDate: order.課程安排資訊!.上課日期![0]!,
+            courseTime: order.課程安排資訊!.上課日期![1]!,
+            // 課程⽇期減 7 天
+            courseDateMinus7: new Date(new Date(order.課程安排資訊!.上課日期![2]!).getTime() - 7 * 24 * 60 * 60 * 1000).toLocaleDateString('zh-TW', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            }),
+            courseLocation: order.課程安排資訊?.教室資訊?.地址,
+            logoSrc: `${process.env.NUXT_PUBLIC_SITE_URL}/about/jie_housekeeper.png`,
+          },
+          {
+            pretty: true,
+          },
+        )
+        const rendered = renderCSS(html, config)
 
-    const rendered = renderCSS(html, config)
+        const { sendMail } = useNodeMailer()
+        const emailResult = await sendMail({
+          subject: `恭喜！您已成功報名中華民國職業認證協會【${order.課程安排資訊?.課程資訊?.課程名稱}】`,
+          html: rendered.html,
+          to: order.會員信箱,
+        })
 
-    const { sendMail } = useNodeMailer()
-
-    const eamilResilt = await sendMail({
-      subject: `恭喜！您已成功報名中華民國職業認證協會【${order.課程安排資訊?.課程資訊?.課程名稱}】`,
-      html: rendered.html,
-      to: order.會員信箱,
-    })
-
-    await notion.blocks.children.append({
-      block_id: order_page_id,
-      children: [{ paragraph: { rich_text: [{ text: { content: JSON.stringify(eamilResilt) } }] } }],
-    })
+        await notion.blocks.children.append({
+          block_id: order_page_id,
+          children: [{ paragraph: { rich_text: [{ text: { content: JSON.stringify(emailResult) } }] } }],
+        })
+      }
+      catch (error) {
+        await notion.blocks.children.append({
+          block_id: order_page_id,
+          children: [{ paragraph: { rich_text: [{ text: { content: JSON.stringify(error) } }] } }],
+        })
+      }
+    }
 
     return '1|OK'
   }
