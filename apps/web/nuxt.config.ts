de967ae10ddada60a33614574abb7f4d0d8bb982
Alan Lu <alan_lu@tk3c.com>
2024-08-12 08:58:02 +0800

    chore: add  telemetry = false

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -8,6 +8,7 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  telemetry: false,
 
   // https://nuxt.com/docs/api/configuration/nuxt-config#modules
   modules: [
