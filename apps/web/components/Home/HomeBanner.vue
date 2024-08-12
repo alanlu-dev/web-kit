407da42ba4b5d8d0582b619cd27633951646ab67
Alan Lu <hi@alanlu.dev>
2024-08-13 02:33:24 +0800

    fix: 單張不輪播

--- apps/web/components/Home/HomeBanner.vue
+++ apps/web/components/Home/HomeBanner.vue
@@ -29,24 +29,35 @@ const ratio = cv(
         <span class="block@tablet hidden">ratio: {{ imgRatio.pc }}</span>
       </div>
 
-      <ClientOnly>
-        <template #fallback>
-          <picture v-if="images">
-            <source media="(max-width: 430px)" :srcset="images[0].圖片_M" />
-            <nuxt-img :src="images[0].圖片_PC" :alt="images[0].標題" :title="images[0].標題" />
-          </picture>
-        </template>
-        <Splide :options="{ arrows: false, autoplay: true, interval: 5000, type: 'loop' }">
-          <SplideSlide v-for="image in images" :key="image.圖片_PC">
-            <component :is="image.導轉連結 ? NuxtLink : 'div'" :to="image.導轉連結 || undefined" :target="image.另開視窗 ? '_blank' : '_self'" class="rel">
-              <picture>
-                <source media="(max-width: 430px)" :srcset="image.圖片_M" />
-                <nuxt-img :src="image.圖片_PC" :alt="image.標題" :title="image.標題" />
-              </picture>
-            </component>
-          </SplideSlide>
-        </Splide>
-      </ClientOnly>
+      <template v-if="images === null || !images?.length">
+        <div>null</div>
+      </template>
+      <template v-else-if="images.length === 1">
+        <picture v-if="images">
+          <source media="(max-width: 430px)" :srcset="images[0].圖片_M" />
+          <nuxt-img :src="images[0].圖片_PC" :alt="images[0].標題" :title="images[0].標題" />
+        </picture>
+      </template>
+      <template v-else>
+        <ClientOnly>
+          <template #fallback>
+            <picture v-if="images">
+              <source media="(max-width: 430px)" :srcset="images[0].圖片_M" />
+              <nuxt-img :src="images[0].圖片_PC" :alt="images[0].標題" :title="images[0].標題" />
+            </picture>
+          </template>
+          <Splide :options="{ arrows: false, autoplay: true, interval: 5000, type: 'loop' }">
+            <SplideSlide v-for="image in images" :key="image.圖片_PC">
+              <component :is="image.導轉連結 ? NuxtLink : 'div'" :to="image.導轉連結 || undefined" :target="image.另開視窗 ? '_blank' : '_self'" class="rel">
+                <picture>
+                  <source media="(max-width: 430px)" :srcset="image.圖片_M" />
+                  <nuxt-img :src="image.圖片_PC" :alt="image.標題" :title="image.標題" />
+                </picture>
+              </component>
+            </SplideSlide>
+          </Splide>
+        </ClientOnly>
+      </template>
     </div>
   </section>
 </template>
