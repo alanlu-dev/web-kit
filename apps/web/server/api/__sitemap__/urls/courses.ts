1da1077707b167775051038b5969cfbd0d5bc5ab
Alan Lu <hi@alanlu.dev>
2024-08-14 04:26:35 +0800

    fix: sitemap 引入取得課程方法

--- apps/web/server/api/__sitemap__/urls/courses.ts
+++ apps/web/server/api/__sitemap__/urls/courses.ts
@@ -1,3 +1,5 @@
+import { getCoursesAsync } from '~/server/service/course/get'
+
 export default defineSitemapEventHandler(async () => {
   const courses = await getCoursesAsync(null, 1, 9999, false, { needCourseEvents: false, needInstructor: false })
   return courses.map((course) => {
