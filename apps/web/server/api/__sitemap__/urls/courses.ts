521162cf4998b2558dc7d437a3c02ffa8f97e210
Alan Lu <alan_lu@tk3c.com>
2024-08-19 13:46:47 +0800

    fix: 補上網站地圖刷新

--- apps/web/server/api/__sitemap__/urls/courses.ts
+++ apps/web/server/api/__sitemap__/urls/courses.ts
@@ -1,7 +1,9 @@
 import { getCoursesAsync } from '~/server/service/course/get'
 
-export default defineSitemapEventHandler(async () => {
-  const courses = await getCoursesAsync(null, 1, 9999, false, { needCourseEvents: false, needInstructor: false })
+export default defineSitemapEventHandler(async (event) => {
+  const refresh = event.node.req.headers['x-prerender-revalidate'] === useRuntimeConfig().vercel.bypassToken
+
+  const courses = await getCoursesAsync(null, 1, 9999, refresh, { needCourseEvents: false, needInstructor: false })
   return courses.map((course) => {
     return {
       _sitemap: 'courses',
