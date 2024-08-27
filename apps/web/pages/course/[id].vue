ccc1df81f1aafb5a681eef02a12d252ea4ffb151
Alan Lu <hi@alanlu.dev>
2024-08-28 05:28:24 +0800

    feat: 下拉選擇

--- apps/web/pages/course/[id].vue
+++ apps/web/pages/course/[id].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { formatThousand } from '@alanlu-dev/utils'
+import type { FormKitOptionsProp } from '@formkit/inputs'
 import type { CourseSchemaType } from '~/schema/course'
 
 const route = useRoute()
@@ -13,12 +14,33 @@ metaStore.updateMeta(route.fullPath, {
   描述: course.value?.課程基礎資訊?.課程特色.join('、'),
   圖片: course.value?.課程照片[0],
 })
+
+const eventFormDate = ref<{ event: number }>({
+  event: course.value?.課程場次資訊?.[0]?.ID || 0,
+})
+
+const eventOptions = computed<FormKitOptionsProp>(() =>
+  !course.value?.課程場次資訊?.length
+    ? [{ label: '尚無課程場次', value: '' }]
+    : [
+        { label: '請選擇場次', value: '' },
+        ...course.value.課程場次資訊!.map((event) => {
+          const limit = event?.指定名額限制 ? event?.指定名額限制 : event?.教室資訊?.名額限制 || 0
+          const currentCount = event?.報名人數 || 0
+
+          const label = `${event?.上課日期?.[0]} ${event?.上課日期?.[1]} ${event?.教室資訊?.名稱} ${event?.已完課 || currentCount >= limit ? '已額滿' : `(${currentCount}/${limit})`}`
+          return { label, value: event?.ID }
+        }),
+      ],
+)
+
+const targetEvent = computed(() => course.value?.課程場次資訊?.find((event) => event?.ID === eventFormDate.value.event))
 </script>
 
 <template>
   <div>
     <Breadcrumb :title="course?.名稱" />
-
+    {{ eventOptions }}
     <section class="{max-w:screen-max;mx:auto}" data-aos="fade-up ">
       <div class="{flex;ai:flex-start;jc:space-between;flex:wrap} {gap:7.5x}@desktop mt:5x text:center">
         <div class="{flex;flex:col;gap:5x} {pr:0;pl:10x}@md flex:1 overflow:hidden px:6x">
@@ -153,34 +175,34 @@ metaStore.updateMeta(route.fullPath, {
               </div>
 
               <div class="{flex;ai:flex-end;jc:space-between;gap:5x;flex:wrap}">
-                <template v-if="course?.課程場次資訊?.[0]">
+                <FormKit v-if="course?.課程型態 !== '免費課程'" v-model="eventFormDate" type="form" :actions="false" form-class="{p:1x}>.formkit-outer">
+                  <FormKit type="select" name="event" :options="eventOptions" />
+                </FormKit>
+                <template v-if="targetEvent">
                   <div v-if="course?.課程型態 !== '免費課程'" class="b1-r {flex;flex:col;gap:2x} {gap:3x}@desktop">
                     <p>
                       <span class="fg:font-title">上課日期：</span>
-                      <span>{{ course?.課程場次資訊?.[0]?.上課日期?.[0] }}</span>
+                      <span>{{ targetEvent?.上課日期?.[0] }}</span>
                     </p>
                     <p>
                       <span class="fg:font-title">上課時間：</span>
-                      <span>{{ course?.課程場次資訊?.[0]?.上課日期?.[1] }}</span>
+                      <span>{{ targetEvent?.上課日期?.[1] }}</span>
                     </p>
                     <p>
                       <span class="fg:font-title">上課地點：</span>
-                      <span>{{ course?.課程場次資訊?.[0]?.教室資訊?.地址 }}</span>
+                      <span>{{ targetEvent?.教室資訊?.地址 }}</span>
                     </p>
                   </div>
-
-                  <div class="{mt:0}@desktop {w:30%}@tablet&<desktop w:full">
-                    <p v-if="course?.課程場次資訊?.[0]?.指定價格" class="h2 nowrap {flex;ai:flex-end;gap:2x;flex:wrap} text:right@tablet&<desktop">
-                      <span class="b1-r fg:divider text:line-through">NT$ {{ formatThousand(course?.價格) }}</span>
-                      <span class="fg:accent">NT$ {{ formatThousand(course?.課程場次資訊?.[0]?.指定價格) }}</span>
-                    </p>
-                    <p v-else class="h2 nowrap fg:accent text:right@tablet&<desktop">NT$ {{ formatThousand(course?.價格) }}</p>
-                    <NuxtLink :to="`/checkout/${course?.課程場次資訊?.[0].ID}`" class="btn btn--primary mt:5x mt:4x@tablet&<desktop w:full">立即報名 </NuxtLink>
-                  </div>
                 </template>
-                <template v-else>
-                  <div class="fg:divider mx:auto">尚無課程場次</div>
-                </template>
+                <div class="{mt:0}@desktop {w:30%}@tablet&<desktop w:full">
+                  <p v-if="targetEvent?.指定價格" class="h2 nowrap {flex;ai:flex-end;gap:2x;flex:wrap} text:right@tablet&<desktop">
+                    <span class="b1-r fg:divider text:line-through">NT$ {{ formatThousand(course?.價格) }}</span>
+                    <span class="fg:accent">NT$ {{ formatThousand(targetEvent?.指定價格) }}</span>
+                  </p>
+                  <p v-else class="h2 nowrap fg:accent text:right@tablet&<desktop">NT$ {{ formatThousand(course?.價格) }} </p>
+                  <NuxtLink v-if="targetEvent?.ID" :to="`/checkout/${targetEvent?.ID}`" class="btn btn--primary mt:5x mt:4x@tablet&<desktop w:full"> 立即報名 </NuxtLink>
+                  <button v-else disabled class="btn btn--primary cursor:no-drop mt:5x mt:4x@tablet&<desktop opacity:.5 w:full"> 立即報名 </button>
+                </div>
               </div>
             </div>
           </div>
