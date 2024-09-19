d65edbe29e5b836ee66917cd256868544cb764ae
Alan Lu <hi@alanlu.dev>
2024-09-19 15:42:46 +0800

    chore(deps): bump package versions

--- apps/nuxt-base/config/modules/elementPlus.ts
+++
@@ -1,7 +0,0 @@
-import type { NuxtModule } from '@nuxt/schema'
-
-// @element-plus/nuxt
-// https://element-plus.org/zh-CN/
-export const nuxtConfig: typeof import('@element-plus/nuxt').default extends NuxtModule<infer O> ? Partial<O> : Record<string, any> = {
-  importStyle: 'scss',
-}
