76b061a92d3bcc41c68a906d5313478eac4ce00c
Alan Lu <hi@alanlu.me>
2024-12-23 17:50:15 +0800

    ci: update CI workflow to include linting and formatting steps

--- apps/nuxt-demo/app/app.vue
+++ apps/nuxt-demo/app/app.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 // import translation from 'zod-i18n-map/locales/zh-TW/zod.json'
-import translation from '~/locales/zod/zh-TW.json'
 import { changeLocale } from '@formkit/i18n'
 import i18next from 'i18next'
 import { ModalsContainer } from 'vue-final-modal'
 import { z } from 'zod'
-
 import { zodI18nMap } from 'zod-i18n-map'
+import translation from '~/locales/zod/zh-TW.json'
 
 useHead({
   htmlAttrs: {
